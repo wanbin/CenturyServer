f552f4db9865350174d1f34c22a5dbbfe61637b9
wensonsmith <wensonsmith@gmail.com>
2012-12-22 23:26:01 +0800

    New Chat System

--- command/ChatGetMessage.php
+++ command/ChatGetMessage.php
@@ -2,52 +2,65 @@
 class ChatGetMessage extends BaseCommand {
 
 	/**
-	 * 添加聊天信息
+	 * 获得聊天信息
 	 *
-	 * @param $params['receiver'] int     	查询用户
-	 * @return array 
+	 * @param $channel 频道ID  0 全部(包括公告)    -1(公告)   > 0 私聊频道
+	 * @return array array('content' => array array('username'=>String ,''content'=> string,'gameuid'=>int,'time'=>int))
 	 */
 	
 	protected function executeEx($params) {
-		if (empty($params['receiver']))
+		if (empty($params['channel']))
 		{
-			$this->throwException("No receiver!", 211);
+			$this->throwException("No channel!", 211);
 		}
-		$receiver = $params['receiver'];
-		          
+
+		$channel = $params['channel'];
+
 		require_once PATH_CACHE . 'ChatCache.php';
+		$chatMC = new ChatCache($this->gameuid);
+		$chatMC->setServer($this->server);
+
+		//如果正在私聊，pull出自己的信息
+		if ($channel > 0 ) {
+			$list = $chatMC->pull($this->gameuid);
+		}else{
+			//否则pull出全部频道的信息
+			$list = $chatMC->pull($channel);
+		}
+
+		//如果是公告
+		if ($channel < 0) {
+			foreach ($list as $key => $value) {
+				if ($value['gameuid'] >0) {
+					unset($list[$key]);
+				}
+			}
+		}
 		
-		$receiverMC = new ChatCache ( $receiver );
-		$receiverMC->setServer($this->server);
-		$resault = array();
-		$resault=$receiverMC->pull();
-		
-		if (count ( $resault ) == 0 && in_array ( $this->gameuid % 10, array (5, 6 ) )) {
+		//如果全部频道没有聊天，开启自动聊天系统（- -！忽悠新手）
+		if (count ( $list ) == 0 && in_array ( $this->gameuid % 10, array (5, 6) ) && $channel = 0) {
 			$loginTime = $this->user_account ['createtime'];
 			if (time () - $loginTime < 60 * 60 * 2)
 			{
-				$resault = array_merge ( $resault, $this->randMessage () );
+				$list = array_merge ( $list, $this->randMessage () );
 			}
 		}
 		
 		
-		
-		if ($receiverMC->isPay && $receiver < 0)
+		//如果开启了付费，他刷新全部频道的时候，那么就要看看他还剩多少免费的聊天
+		if ($chatMC->isPay && $channel = 0)
 		{
-			$chatMC = new ChatCache($this->gameuid);
-			$chatMC->setServer($this->server);
 			$surplus = $chatMC->getSurplus();
-			$resault['surplus'] = $surplus;
+			$list['surplus'] = $surplus;
 		}else{
-			$resault['surplus'] = 1;
+			$list['surplus'] = 1;
 		}
 		
-		
-		return $resault;
+		return $list;
 	
 	}
 	
-	function randMessage()
+	private function randMessage()
 	{
 		$nameArr = array('有美','朱君','愤怒的小白','皇帝','荷花','正太','拖鞋','雄治','杏樹','美姫','香','趙子龍','三国');
 		$messageArr =array('求加好友','谁有A卡','快来解救我','大家多少级了？','我有B级吕布~~~~','有没有人有A卡？','我15级啊','在哪能找到你？？','推图推失败了，怎么了？？？','大家这么活跃~~','游戏很有意思啊');
--- command/ChatSendMessage.php
+++ command/ChatSendMessage.php
@@ -7,9 +7,9 @@ class ChatSendMessage extends BaseCommand {
 	
 	/**
 	 * 添加聊天信息
-	 * @param $params['receiver'] int      接受者
+	 * @param $receiver 0 全部(包括公告) gameuid(与此人的私聊)
 	 * @param $params['content'] string    聊天信息
-	 * @return array 聊天数据和免费余量
+	 * @return array()       上次刷新到现在新增的聊天记录   
 	 */
 	
 	protected function executeEx($params)
@@ -29,20 +29,15 @@ class ChatSendMessage extends BaseCommand {
 		$receiver = $params['receiver'];
 		$content=$this->sec($params['content']);
 		
-		$message = array(
-					'gameuid' => $this->gameuid,
-					'displayname' => $this->user_account['displayname'],
-					'content' => $content
-				);
-		
 		require_once PATH_CACHE . 'ChatCache.php';
-		$receiverMC = new ChatCache($receiver);
-		
+		$chatMC = new ChatCache( $this->gameuid);
+		$chatMC->setServer($this->server);
+		$message = $chatMC->createMessage($this->gameuid, $this->user_account['displayname'], $content);
+
 		//全部频道付费时
-		if ($receiver < 0 && $receiverMC->isPay)
+		if ($receiver < 0 && $chatMC->isPay)
 		{
-			$chatMC=new ChatCache( $this->gameuid);
-			$chatMC->setServer($this->server);
+			
 			$surplus = $chatMC->getSurplus();
 			if ($surplus < 0){
 				$points=array('points' => $this->cost);
@@ -55,17 +50,28 @@ class ChatSendMessage extends BaseCommand {
 			$surplus = 1;
 		}
 		
-		$receiverMC->setServer($this->server);
-		$receiverMC->push($message);
+		// push $message To $receiver
+		$chatMC->push($message,$receiver);
 		
+		//如果是私聊,那么也需要给自己push一份
+		if ($receiver > 0) {
+			$chatMC->push($message,$this->gameuid);
+		}
 		if($this->logText)
 		{
 			$this->writeText($message);
 		}
-		$resault = array();
-		$resault = $receiverMC->pull();
-		$resault['surplus'] = $surplus;
 		
+		$resault = array();
+		//如果是私聊，则pull出自己的记录
+		if ($receiver > 0){
+			$resault = $chatMC->pull($this->gameuid);
+		}else{
+			//如果是全部频道，则pull出全部频道的内容
+			$resault = $chatMC->pull($receiver);
+		}
+		
+		$resault['surplus'] = $surplus;
 		return $resault;
 	}
 
