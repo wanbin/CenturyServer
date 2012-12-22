d7736a9ed8754ea674857ae2fe17fb0386171364
wensonsmith <wensonsmith@gmail.com>
2012-12-22 23:31:45 +0800

    New chatcache

--- dataobj/cache/ChatCache.php
+++ dataobj/cache/ChatCache.php
@@ -7,145 +7,137 @@ class ChatCache extends ChatModel
 	private $limit = 3;  //每次返回长度
 	private $free  = 5;  //免费条数
 	private $list = array();
-	public  $isPay = true ;
-
+	public  $isPay = true ;     //是否开启付费
 	
-	//推入内容
-	public function push($content)
+	private $cacheMC = NULL;
+	private $userid  = NULL;
+	
+	public function __construct($gameuid)
 	{
-		$node = $this->createNode($content);
-		//设置指针数据
-		$this->setPointer();
-		$key  = $this->getCacheKey('node');
-		$this->setToCache($key, $node,3600,$this->gameuid);
-	}
-	
-	
-	//拉取列表
-	public function pull()
-	{
-		$key  = $this->getCacheKey('node');
-		$node = $this->getFromCache($key, $this->gameuid);
-		$this->list[]=$node['data'];
-		$count = 1;
-		while($node['next'] && $count < $this->limit)
-		{
-			$node = $this->getFromCache($node['next'], $this->gameuid);
-			echo $node['next'];
-			if (!empty($node))
-			{
-				$this->list[] = $node['data'];
-				$count++;
-			}
-			
+		parent::__construct($gameuid);
+		if ( empty($this->cacheMC) ) {
+			$this->cacheMC = parent::getCacheInstance('cache_chat');
 		}
 		
-		return $this->list;
 	}
 	
-	//生成结点
-	protected function createNode($content)
+	public function createMessage($gameuid,$name,$content)
 	{
-		$next = $this->getCacheKey('node');
-		$node = array(
-					'data' => $content,
-					'next' => $next
+		$message = array(
+					'gameuid' => $gameuid,
+					'displayname' => $name,
+					'content' => $content,
+					'time'    => time()
 				);
-		return $node;
+		return $message;
 	}
 	
+	/*
+	* push ($message,$receiver)  把$message 发给 $receiver
+	*/
+	public function push($message,$receiver)
+	{
+		$this->userid = $receiver;
+		$cacheKey = $this->getCacheKey('node');
+		$list = $this->cacheMC->get($cacheKey);
+		
+		$length = count($list);
+		while ($length >= $this->max)
+		{
+			array_shift($list);
+			$length--;
+		}
+		$list[] = $message;
+
+		$this->cacheMC->set($cacheKey,$list);
+		
+		// return $message;
+		
+	}
+	
+
+	/*
+	*  pull($userid)  获取$userid 的消息
+	*/
+	public function pull($userid)
+	{
+		$this->userid = $userid;
+		$cacheKey = $this->getCacheKey('node');
+		$list = $this->cacheMC->get($cacheKey);
+
+		$lastTime = $this->getLastTime();
+
+		foreach ($list as $key => $value) {
+			if ($value['time']<$lastTime) {
+				unset($list[$key]);
+			}
+		}
+		return $list;
+	}
 	
 	protected function getCacheKey($key)
 	{
 		switch($key)
 		{
-			case 'pointer':
-				return sprintf(MEMCACHE_KEY_POINTER_SERVER_GAMEUID,$this->server,$this->gameuid);
-				break;
 			case 'surplus':
 				return sprintf(MEMCACHE_KEY_SURPLUS_SERVER_GAMEUID,$this->server,$this->gameuid);
 				break;
 			case 'node':
-				$position = $this->getPosition();
-				if ($position === 0)
-				{
-					$position = $this->max;
-				}
-				return sprintf(MEMCACHE_KEY_NODE_SERVER_GAMEUID_POSITION,$this->server,$this->gameuid,$position);
+				return sprintf(MEMCACHE_KEY_NODE_SERVER_GAMEUID,$this->server,$this->userid);
 				break;
-			
+			case 'time':
+				return sprintf(MEMCACHE_KEY_LASTTIME_SERVER_GAMEUID,$this->server,$this->gameuid);
 			default:break;
 		}
 		
 	}
-	
-	//获取指针位置
-	protected function getPosition()
-	{
-		$key = $this->getCacheKey('pointer');
-		$position = $this->getFromCache($key, $this->gameuid);
-		
-		//初始化情况
-		if (empty($position))
-		{
-			$position = 0;
-		}
-		
-		//队列满员情况
-		if ($position > $this->max)
-		{
-			$position = $position % $this->max;
-		}
-		return $position;
-	}
-	
-	
-	//设置指针
-	protected function setPointer()
-	{
-		$position = $this->getPosition();
-		$key = $this->getCacheKey('pointer');
-		
-		$position ++;
-		$this->setToCache($key, $position,3600, $this->gameuid);
-	}
-	
-	
-	//获取余量
-	public function getSurplus()
-	{
-		$key = $this->getCacheKey('surplus');		
-		$res = $this->getFromCache($key, $this->gameuid);
 
-		if(empty($res))
+	//获取玩家上次刷新时候的时间
+	private function getLastTime()
+	{
+		$cacheKey = $this->getCacheKey('time');
+		$lastTime = $this->cacheMC->get($cacheKey);
+		if (empty($lastTime)) {
+			$lastTime = 0 ;
+		}
+		$this->cacheMC->setToCache($cacheKey,time(),3600,$this->gameuid);
+		return $lastTime;
+	}
+		
+	// 获取余量
+	public function getSurplus() 
+	{
+		$key = $this->getCacheKey ( 'surplus' );
+		$res = $this->cacheMC->get( $key );
+		
+		if (empty ( $res )) 
 		{
-			$res = parent::getSurplus($this->gameuid);
-			$this->setToCache($key, $res,3600, $this->gameuid);
+			$res = parent::getSurplus ( $this->gameuid );
+			$this->cacheMC->setToCache ( $key, $res, 3600, $this->gameuid );
 		}
 		
-		if($res['chatdate'] != date('Ymd'))
+		if ($res ['chatdate'] != date ( 'Ymd' )) 
 		{
 			$surplus = $this->free;
-		}else{
-			$surplus = $this->free - $res['surplus'];
+		} else {
+			$surplus = $this->free - $res ['surplus'];
 		}
 		return $surplus;
 	}
 	
-	
-	//设置免费余量
-	public function setSurplus($surplus)
+	// 设置免费余量
+	public function setSurplus($surplus) 
 	{
-		$key = $this->getCacheKey('surplus');
-		$res['chatdate'] = date('Ymd');
-		$res['surplus']  = $this->free-$surplus+1;
-		if ($res['surplus'] == $this->free+1)
-		{
-			parent::updateChatCounter($this->gameuid);
+		$key = $this->getCacheKey ( 'surplus' );
+		$res ['chatdate'] = date ( 'Ymd' );
+		$res ['surplus'] = $this->free - $surplus + 1;
+		if ($res ['surplus'] == $this->free + 1)
+		 {
+			parent::updateChatCounter ( $this->gameuid );
 		}
-		$this->setToCache($key, $res,3600, $this->gameuid);
-	}
+		$this->cacheMC->setToCache ( $key, $res, 3600, $this->gameuid );
+	}	
 	
-	
+
 }
 ?>
