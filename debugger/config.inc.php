513547a9c74f71599057127c0587ef96ea7a6995
万斌 <wanbin89731@163.com>
2012-12-28 01:01:11 +0800

    account

--- debugger/config.inc.php
+++ debugger/config.inc.php
@@ -1,9 +1,9 @@
 <?php
-$uid = 'yo_8';
-$gameuid =40330;
+$uid = 'wanbin';
+$gameuid = rand(1,9000);
 $pid = '1';
 $authCode =14245730;
 $server = 1;
 $version = '1.4.0';
-$session_key = "0001505c0c4c0058c8a19d44d38e1d8d178ecbe411e07ce067c98c2b276a8552ea5c97704bfd53c32feeace6722fed2a104993b36ac06113dc40712a4be5cd464e5fbee6f25958c3ab94d89c531f87d606bf88c8056ea5e82cd00ac2e61408de";
+$session_key = "";
 ?>
