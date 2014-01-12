d0457d5ea217952815c61eb64f4fbb7ab4a7a13b
wanbin <wanbin89731@163.com>
2014-01-13 00:33:45 +0800

    记录用户登录LOG

--- command/UserGetInfo.php
+++ command/UserGetInfo.php
@@ -2,6 +2,7 @@
 // 新建一条记录
 include_once 'BaseCommand.php';
 include_once 'handler/AccountHandler.php';
+include_once 'handler/BehaveHandler.php';
 class UserGetInfo extends BaseCommand {
 	protected function executeEx($params) {
 		$uid = $params ['uid'];
@@ -10,6 +11,11 @@ class UserGetInfo extends BaseCommand {
 		}
 		$account = new AccountHandler ( $this->uid );
 		$ret= $account->getAccountByUid($uid);
+		
+		
+		$behavea = new BehaveHandler ( $this->uid );
+		$behavea->newBehave ( "getUserInfo","","" );
+		
 		return $this->reutrnDate ( COMMAND_ENPTY ,$ret);
 	}
 }
