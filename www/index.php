b795adf32f300ba84f419eabc789bcedafaaf950
斌万 <497442643@qq.com>
2014-08-14 00:15:45 +0800

    首页

--- www/index.php
+++ www/index.php
@@ -1,6 +1,5 @@
 <?php
 include_once '../define.php';
-
 require_once PATH_ROOT."framework/MooPHP/MooPHP.php";
 
 include_once PATH_CONTROL.'/header.php';
@@ -9,15 +8,15 @@ include( Mootemplate( 'header' ) );
 
 $showPage=$_REQUEST['showpage'];
 
-$pageArr=array('help');
+$pageArr=array('help','home');
 
 if(in_array($showPage, $pageArr)){
 	include_once PATH_CONTROL."/$showPage.php";
 	include( Mootemplate( $showPage) );
 }
 else{
-	include_once PATH_CONTROL.'/home.php';
-	include( Mootemplate( 'home' ) );
+	include_once PATH_CONTROL.'/index.php';
+	include( Mootemplate( 'index' ) );
 }
 
 
