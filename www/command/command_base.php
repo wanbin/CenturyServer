6d251fda742128d0b2dba7f56004cb8f3e87ecf0
斌万 <497442643@qq.com>
2014-08-23 19:43:38 +0800

    最后小调整

--- www/command/command_base.php
+++ www/command/command_base.php
@@ -1,11 +1,16 @@
 <?php
+session_start ();
 include '../../define.php';
 $modle = $_REQUEST ['mod'];
 $action = $_REQUEST ['action'];
 
-$uid = isset ( $_REQUEST ['username'] ) ? $_REQUEST ['username']  : "";
-print_R($_REQUEST);
-print_r($_SESSION['username']);
+if (isset ( $_SESSION ['username'] )) {
+	$uid=$_SESSION ['username'];
+}else{
+	$uid = isset ( $_REQUEST ['username'] ) ? $_REQUEST ['username']  : "";
+}
+
+
 if(!empty($uid)){
 	include_once PATH_VIEW_COMMAND . '/command_' . $modle . '_' . $action . '.php';
 }
