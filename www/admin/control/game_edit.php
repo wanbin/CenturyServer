79ace14ae578541c79d683724bef1f03878d6323
斌万 <497442643@qq.com>
2014-12-31 15:10:35 +0800

    显示缩略图

--- www/admin/control/game_edit.php
+++ www/admin/control/game_edit.php
@@ -5,5 +5,8 @@ $ret=$game->getGameList(1);
 foreach ($ret as $key=>$value){
 	$ret[$key]['showtime']=date("Y-m-d H:i:s",$value['showtime']);
 	$ret[$key]['status']=$value['showtime']>time()?"未展示":"已经展示";
+	if(!empty($value['homeimg'])){
+		$ret[$key]['homeimg'].='!50X50';
+	}
 }
 
