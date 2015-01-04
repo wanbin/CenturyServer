84da4efcadb85db8177b33a038f81c52d5605e7c
斌万 <497442643@qq.com>
2015-01-05 00:25:28 +0800

    游戏图标

--- www/control/game_list.php
+++ www/control/game_list.php
@@ -5,6 +5,9 @@ $game = new GameHandler ($uid,'WEB');
 $ret=$game->getGameList(1);
 foreach ($ret as $key=>$value){
 	$ret[$key]['showtime']=date("Y-m-d",$value['showtime']);
+	if(!empty($value['homeimg'])){
+		$ret[$key]['homeimg'].='!50X50';
+	}
 }
 
 ?>
