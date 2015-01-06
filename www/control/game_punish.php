b18cd0c74019e2aa4b86bfae1aca38e994a8f5fd
斌万 <497442643@qq.com>
2015-01-07 01:10:12 +0800

    啥也不说啦

--- www/control/game_punish.php
+++ www/control/game_punish.php
@@ -3,6 +3,6 @@ $page=isset($_REQUEST['page'])?$_REQUEST['page']:1;
 // echo $page;
 include_once PATH_HANDLER . 'PunishHandler.php';
 $punish = new PunishHandler ( $uid );
-$ret = $punish->getPageList ( $page,0 );
+$ret = $punish->getPageList ( $page,-1 );
 
 ?>
