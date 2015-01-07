bf5ec423929dc10e91c152756d5df59d47556f60
斌万 <497442643@qq.com>
2015-01-07 15:41:53 +0800

    获取列表的时候能获取帮助

--- command/GameList.php
+++ command/GameList.php
@@ -5,9 +5,10 @@ include_once 'handler/ArticleHandler.php';
 class GameList extends BaseCommand {
 	protected function executeEx($params) {
 		$page = isset($params ['page'])?$params ['page']:1;
+		$showtype = isset($params ['type'])?$params ['type']:1;
 		//是否是需要审核的词汇
 		$game = new ArticleHandler ( $this->uid );
-		$ret = $game->getGameList ( $page,1 );
+		$ret = $game->getGameList ( $page, $showtype );
 		return $this->reutrnDate ( COMMAND_ENPTY, $ret );
 	}
 
