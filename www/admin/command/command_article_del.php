dc9f197e370d5e4f50335ba17d741d95d126ce03
斌万 <497442643@qq.com>
2015-01-07 23:29:22 +0800

    删除

--- www/admin/command/command_article_del.php
+++ www/admin/command/command_article_del.php
@@ -2,7 +2,7 @@
 //读某个人的信息，把这个人的未读标记去掉
 $id=$_REQUEST["_id"];
 
-include_once PATH_HANDLER.'GameHandler.php';
+include_once PATH_HANDLER.'ArticleHandler.php';
 $game=new ArticleHandler($uid);
 $game->delGame($id);
 echo "删除成功,ID:".$id;
