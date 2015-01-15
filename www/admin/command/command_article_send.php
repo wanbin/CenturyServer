8287e3983b89c5747f0a7a4a0ce98745a95b88c6
斌万 <497442643@qq.com>
2015-01-15 21:29:20 +0800

    发文章直接发推送

--- www/admin/command/command_article_send.php
+++ www/admin/command/command_article_send.php
@@ -29,4 +29,19 @@ $homeurl=$matchs[0][0];
 
 $id=$game->newGame($title, $homeurl, $content, $timeint,$type,$keyname);
 
+
+if ($type == 1) {
+	$pushcontent = "最新游戏 $title 已经上线，快来围观吧！爱上聚会";
+	// 读某个人的信息，把这个人的未读标记去掉
+	include_once '../model/PushBase.php';
+	$push = new PushBase ();
+	$id = $push->newpush ( $pushcontent, $timeint );
+}
+
 echo $content."发送成功,ID:".$id;
+
+
+
+
+
+
